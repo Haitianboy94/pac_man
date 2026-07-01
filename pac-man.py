482bf3a9d18b46babb08fa93ae64ebd7592b91a1
Robbin Ploeger <robbplo@gmail.com>
2026-07-01 14:35:19 +0200

    Ball demo

--- pac-man.py
+++ pac-man.py
@@ -1,2 +1,28 @@
 if __name__ == "__main__":
-    print("pacman goes here")
+    import sys, pygame
+    pygame.init()
+
+    size = width, height = 320, 240
+    speed = [2, 2]
+    black = 0, 0, 0
+    clock = pygame.time.Clock()
+
+    screen = pygame.display.set_mode(size)
+
+    ball = pygame.image.load("intro_ball.gif")
+    ballrect = ball.get_rect()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+
+        ballrect = ballrect.move(speed)
+        if ballrect.left < 0 or ballrect.right > width:
+            speed[0] = -speed[0]
+        if ballrect.top < 0 or ballrect.bottom > height:
+            speed[1] = -speed[1]
+
+        screen.fill(black)
+        screen.blit(ball, ballrect)
+        pygame.display.flip()
+        clock.tick(60)  # limits FPS to 60
---
+++ src/config.py
@@ -0,0 +1,15 @@
+from dataclasses import dataclass
+
+@dataclass
+class Config():
+    highscore_filename: str = "highscore.json"
+    level: list[int] = [1,2,3,4,5,6,7,8,9,10]
+    width: int = 50
+    height: int = 50
+    lives: int = 3
+    pacgum: int = 42
+    points_per_pacgum: int = 10
+    points_per_super_pacgum: int = 50
+    points_per_ghost: int = 200
+    seed: str = "42"
+    level_max_time: int = 90
