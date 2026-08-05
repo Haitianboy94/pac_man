dedf7e5ae66633bec9a8816821454c72e99641f8
Robbin Ploeger <robbplo@gmail.com>
2026-08-05 12:08:28 +0200

    Add animation class

--- src/entities/player.py
+++ src/entities/player.py
@@ -1,3 +1,4 @@
+from src.graphics.animation import Animation
 from src.graphics.general_sprites import GeneralSprites
 from src.types import Dir
 from src.entities.entity import Entity
@@ -10,38 +11,28 @@ class Player(Entity):
     The pacman player entity
     """
     SIZE = 16
-
     FPS = 16
 
     def __init__(self, sprites: GeneralSprites) -> None:
         pg.sprite.Sprite.__init__(self)
 
-        self.current_frame: int = 0
-        self.frame_elapsed_ms: int = 0
         self.rect = pg.Rect(0, 0, self.SIZE, self.SIZE)
         self.sprites = sprites
 
         self.direction: Dir = Dir.EAST
-        self.frames = self.sprites.player_moving_east()
-        self.image = self.frames[0]
-
-    def update_frame(self, dt: int):
-        ms_per_frame: int = int((1 / self.FPS) * 1000)
-
-        self.frame_elapsed_ms += dt
-        if self.frame_elapsed_ms > ms_per_frame:
-            self.frame_elapsed_ms -= ms_per_frame
-            self.current_frame += 1
-            self.current_frame %= len(self.frames)
-            self.image = self.frames[self.current_frame]
+        self.move_animations: dict[Dir, Animation] = {
+            Dir.NORTH: Animation(self.sprites.player_moving_north(), self.FPS),
+            Dir.EAST: Animation(self.sprites.player_moving_east(), self.FPS),
+            Dir.SOUTH: Animation(self.sprites.player_moving_south(), self.FPS),
+            Dir.WEST: Animation(self.sprites.player_moving_west(), self.FPS),
+        }
+        self.animation = self.move_animations[self.direction]
+        self.image = self.animation.image
 
     def update(self, dt: int) -> None:
-        self.update_frame(dt)
+        self.animation.update_frame(dt)
+        self.image = self.animation.image
 
     def set_direction(self, direction: Dir) -> None:
         self.direction = direction
-        match direction:
-            case Dir.NORTH: self.frames = self.sprites.player_moving_north()
-            case Dir.EAST: self.frames = self.sprites.player_moving_east()
-            case Dir.SOUTH: self.frames = self.sprites.player_moving_south()
-            case Dir.WEST: self.frames = self.sprites.player_moving_west()
+        self.animation = self.move_animations[self.direction]
